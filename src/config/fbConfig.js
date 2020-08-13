a272b3d3f58bf0f88e96876a210609585857302c
Michael Hicks <kustomdesigner@Michaels-MacBook-Pro.local>
2020-08-13 14:15:06 -0400

    Add firebase store - Stuck on error

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCOuSEzaY5k1sGUKhshVJjZJDoE7IcsGJg",
+    authDomain: "website-url-checker.firebaseapp.com",
+    databaseURL: "https://website-url-checker.firebaseio.com",
+    projectId: "website-url-checker",
+    storageBucket: "website-url-checker.appspot.com",
+    messagingSenderId: "316213442724",
+    appId: "1:316213442724:web:bdd24092d581676e5a4e31",
+    measurementId: "G-QL5LZDVJ68"
+  };
+
+  firebase.initializeApp(config);
+  firebase.fireStore().settings({ timestampsInSnapshots: true });
+
+  export default firebase;
--- src/store/actions/websiteActions.js
+++ src/store/actions/websiteActions.js
@@ -1,5 +1,5 @@
 export const addWebsite = (website) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
       // Call to DB
       // Dispatch Action
       dispatch({ type: 'ADD_WEBSITE', website: website });
