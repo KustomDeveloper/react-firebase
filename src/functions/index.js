fddfdaec18ba34cfdeacab3fbaa7a8b913f8d9c3
Michael Hicks <kustomdesigner@gmail.com>
2020-08-27 18:01:41 -0400

    Added more cloud functions for user notifications and website creation notifications

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,12 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+}
+
+exports.websiteCreated = functions.firestore
+.document('websites/{websiteId}')
+.onCreate(doc => {
+  const website = doc.data();
+  const notification = {
+    content: 'Added a new website',
+    website: `${website.url}`,
+    time: admin.firestore.FieldValue.serverTimestamp()
+  }
+
+  return createNotification(notification);
+});
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+    return admin.firestore()
+                .collection('users')
+                .doc(user.uid)
+                .get()
+                .then(doc => {
+                  const newUser = doc.data();
+                  const notification = {
+                    content: 'New User Signed Up',
+                    user: `${newUser.firstName} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                  }
+                  return createNotification(notification);
+                })
+})
